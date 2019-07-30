1d85d5b1c55e05dd46351f70c36312deffb57e6e
liwei <491520313@qq.com>
2019-07-30 20:28:33 +0800

    add oss service

--- src/services/oss.js
+++ src/services/oss.js
@@ -2,27 +2,60 @@
 
 const OSS = require('ali-oss');
 const client = new OSS({
-  region: 'oss-cn-beijing',
-  accessKeyId: 'LTAIjY0NjBpEkpUR',
-  accessKeySecret: 'hcbqkZADWy0GflyVMsHoXPiRtL6nIM',
-  bucket: 'xwjshopdev',
-  endpoint: 'oss-cn-beijing.aliyuncs.com'
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAIbwRQ4jstYPzJ',
+    accessKeySecret: 'fgnbbK1TN3buyaRqrYAcr67EngALVU',
+    bucket: 'yumimiao'
 });
 
 async function Upload(files, {getFileName, dir})
 {
     //上传图片
     let n = files.length;
-    let ok = true;
+    let result = [];
     for(let i=0;i<n;i++) {
         let file = files[i];
         let ret =  await  client.put(dir + getFileName(file), file);
-        ok = ok && ret.res.status == 200;
-        if( !ok ) {
-            console.error(`文件${file.name}上传失败`);
+        if(ret.res.status == 200)
+        {
+            result.push( ret.res.url );
+            continue;
+        }
+        console.error(`文件${file.name}上传失败`);
+    }
+    return result;
+}
+
+function getFileExt(name)
+{
+    let arr = name.split('.');
+    if( arr.length < 1) return "";
+    return arr[arr.length-1];
+}
+
+async function uploadAvatar(file)
+{
+    let dbid =  localStorage.getItem('dbid');
+    
+    if( !dbid ) {
+
+        window.Bus.$emit('Login401');
+        
+        return {
+            errorNo:401,
+            errorDesc:"用户未登录！",
+            result:null
         }
     }
-    return ok;
+
+    //根据dbid获取上传图片的token
+    let ret = await Upload(file,{dir: dbid +'/images/avatar/', getFileName:(name)=>{Date.now() + getFileExt(name)}});
+    let ok = ret.length == 1;
+    return {
+        errorNo:ok?200:400,
+        errorDesc:ok?"图片上传成功！":"图片上传失败！",
+        result:ok?ret[0]:null
+    }
 }
 
 
