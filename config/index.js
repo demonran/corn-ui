4b802d2c637979eb483d7c3d98ffd73e308f14de
demonran <445999306@qq.com>
2020-02-26 15:02:11 +0800

    add local debug

--- config/index.js
+++ config/index.js
@@ -5,14 +5,24 @@
 const path = require('path')
 
 module.exports = {
+  local: {
+    proxyTable: {
+      '/api-yumimiao': {
+        target: 'http://localhost:8082', // 你请求的第三方接口
+        changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: {  // 路径重写，
+          '^/api-yumimiao': ''  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+        }
+      }
+    }
+  },
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
       '/api-yumimiao': {
-        target:'http://47.106.127.210/api-yumimiao', // 你请求的第三方接口
+         target:'http://47.106.127.210/api-yumimiao', // 你请求的第三方接口
         changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite:{  // 路径重写，
             '^/api-yumimiao': ''  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
